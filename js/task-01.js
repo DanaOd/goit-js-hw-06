284e4e17356b6d72340575558c061166c06bcd50
Dana <dana.lalovaod@gmail.com>
2023-03-01 21:19:40 +0200

    feat: add second task

--- js/task-01.js
+++ js/task-01.js
@@ -5,19 +5,15 @@ let subCategories = categoriesListEl.children;
 console.log(`Number of categories: ${subCategories.length}
     `);
 
-
-
 //2. Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента
 // (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
 const subCategoriesArr = [...subCategories];
 
 subCategoriesArr.forEach(listItem => {
+  const titleEl = listItem.querySelector('h2');
+  console.log(`Category: ${titleEl.textContent}`);
 
-    const titleEl = listItem.querySelector('h2');
-    console.log(`Category: ${titleEl.textContent}`);
-
-    const elementsEl = listItem.querySelectorAll('li');
-    console.log(`Elements: ${elementsEl.length}
+  const elementsEl = listItem.querySelectorAll('li');
+  console.log(`Elements: ${elementsEl.length}
     `);
-
 });
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
+// Добавит название ингредиента как его текстовое содержимое.
+// Добавит элементу класс item.
+// После чего вставит все <li> за одну операцию в список ul#ingredients.
+
+let ingredientsListRef = document.querySelector('#ingredients');
+
+
+const ingredientsListItems = ingredients.map(ingredient=>{
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add('item')
+
+  return listItem;
+});
+
+ingredientsListRef.append(...ingredientsListItems);
+console.log(...ingredientsListItems);
+
