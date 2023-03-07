be9ae6936735b727ac24476cd94983c7a7df8c8b
Dana <dana.lalovaod@gmail.com>
2023-03-07 20:58:10 +0200

    feat: add 9 task

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  colorName: document.querySelector('.color'),
+  buttonChangeColor: document.querySelector('.change-color'),
+};
+
+refs.buttonChangeColor.addEventListener('click', onClickHandler);
+
+function onClickHandler(event) {
+  console.log(
+    'sayHi',
+    getRandomHexColor(),
+    document.body.style.backgroundColor,
+  );
+  let randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  refs.colorName.textContent = randomColor;
+}
