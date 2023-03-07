bd3000ffefd7d387afcd1af76280c1f13e1c967c
Dana <dana.lalovaod@gmail.com>
2023-03-07 16:52:52 +0200

    feeat: add 78 task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+  email: document.querySelector("[name='email']"),
+  password: document.querySelector("[name='password']"),
+};
 
+const credentials = {};
+
+refs.form.addEventListener('submit', onSubmitHandler);
+
+function onSubmitHandler(event) {
+  event.preventDefault();
+
+  let emailValue = event.currentTarget.elements.email.value;
+  let passwValue = event.currentTarget.elements.password.value;
+
+  if (emailValue === '' || passwValue === '') {
+    return console.log('Please fill in all the fields!');
+  } else {
+    credentials.email = emailValue;
+    credentials.password = passwValue;
+    console.log(credentials);
+    refs.email.value = '';
+    refs.password.value = '';
+  }
+}
