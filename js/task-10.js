ed5b6312e42bdbcd2120a527ba7fcbef68b9db79
Dana <dana.lalovaod@gmail.com>
2023-03-07 21:49:35 +0200

    feat: add 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  container: document.querySelector('#boxes'),
+  input: document.querySelector('[type="number"]'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+};
+
+refs.createBtn.addEventListener('click', createBoxes);
+let markUp = '';
+
+function createBoxes(amount) {
+  amount = refs.input.value;
+  let width = 30;
+  let height = 30;
+
+  for (let i = 1; i <= amount; i += 1) {
+    markUp += `<div style="width:${width}px; height:${height}px; background-color:${getRandomHexColor()};"></div>`;
+    width += 10;
+    height += 10;
+  }
+
+  refs.container.insertAdjacentHTML('afterBegin', markUp);
+}
+
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  refs.container.innerHTML = '';
+}
